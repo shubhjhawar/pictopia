7b0f39bd3f8e3bfd39d0d44aae493e9ea7e52834
Shubh <shubhjhawar78@gmail.com>
2023-06-16 09:36:30 -0400

    started working on the homepage

--- pictopia_frontend/src/container/Home.jsx
+++ pictopia_frontend/src/container/Home.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react';
+import {HiMenu} from 'react-icons/hi';
+import {AiFillCloseCircle} from 'react-icons/ai';
+import {Link, Route, Routes } from 'react-router-dom';
+
+// import Sidebar from "../components/Sidebar";
+// import UserProfile from "../components/UserProfile";
+import { Sidebar, UserProfile } from '../components';
+import {client} from "../client";
+import logo from "../assets/logo.png";
+
+import Pins from "./Pins";
+
+
 
 const Home = () => {
+  const [toggleSidebar, settoggleSidebar] = useState(false)
+
+
+
   return (
-    <div>Home</div>
+    <div className='flex bg-gray-50 md:flex-row flex flex-col h-screen transition-height duration-75 ease-out'>
+      <div className='hidden md:flex h-screen flex-initial'>
+        <Sidebar />
+      </div>
+      <div className="flex md:hidden flex-row">
+        <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(false)}/>
+      </div>
+    </div>
   )
 }
 
