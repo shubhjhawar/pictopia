f2bed27c01eb00e65ae67e7cb5825737b94463cf
Shubh <shubhjhawar78@gmail.com>
2023-06-19 07:13:02 -0400

    started working on create-pin section

--- pictopia_frontend/src/components/CreatePin.jsx
+++ pictopia_frontend/src/components/CreatePin.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React,{useState} from 'react';
+import {AiOutlineCloudUpload} from 'react-icons/ai';
+import { MdDelete } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 
-const CreatePin = () => {
+import {client} from "../client";
+import Spinner from "./Spinner";
+
+
+const CreatePin = ({user}) => {
   return (
     <div>CreatePin</div>
   )
